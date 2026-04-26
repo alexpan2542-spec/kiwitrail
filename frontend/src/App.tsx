13c4f88e16ff654a60b6607761fc11395d683cac
Alex <alexpan2542@gmail.com>
2026-04-27 08:25:51 +1200

    update frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,5 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import TrackDetailPage from "./pages/TrackDetailPage";
-import HomePage6 from "./pages/HomePage6";
-import HomePage7 from "./pages/HomePage7";
-import HomePage5 from "./pages/HomePage5";
 import HomePage from "./pages/HomePage";
 import Track from "./pages/Track";
 import MyMap from "./pages/MyMap";
@@ -14,9 +11,6 @@ function App() {
         <Route path="/my-map" element={<MyMap />} />
         <Route path="/track/:trackId" element={<Track />} />
         <Route path="/" element={<HomePage />} />
-        <Route path="/5" element={<HomePage5 />} />
-        <Route path="/6" element={<HomePage6 />} />
-        <Route path="/7" element={<HomePage7 />} />
         <Route path="/track/:id" element={<TrackDetailPage />} />
       </Routes>
     </BrowserRouter>
